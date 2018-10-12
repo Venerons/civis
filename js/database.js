babe0ba5b180ce5cc20b0f7479984df70bc26aca
Daniele Veneroni <Venerons@users.noreply.github.com>
2018-10-12 16:07:54 +0200

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,115 +1,241 @@
-// Copyright (c) 2013-2015 Daniele Veneroni.
-// Released under GPLv3 License. See LICENSE.md for further information.
+var DATABASE = {
 
-'use strict';
+	// CIVILIZATIONS
 
-// ##############################################
-// # UNITS                                      #
-// ##############################################
-/*
-
-Unit-name-archer "Arciere"
-Unit-desc-archer "questa descrizione comparirà nel campo title dove serve ecc."
-
-*/
-
-window.UNITSDB = {
-	'warrior' : {
-		type: 'land', // land, sea, air
-		strength: 1,
-		toughness: 1,
-		movement: 1,
-		cost: 2,
-		require: null,
-		obsolete: null
-	}
-};
-
-// ##############################################
-// # BUILDINGS                                  #
-// ##############################################
-/*
-
-Building-name-granary "Granaio"
-Building-desc-granary "questa descrizione comparirà nel campo title dove serve ecc."
-
-*/
-
-window.BUILDINGSDB = {
-	'granary': {
-		cost: 10, // production cost
-		requireTech: 'XXXXX', // tech required
-		requireBuild: 'XXXX', // building required
-		maintenance: 1 //maintenance cost. (to be removed, and fix a constant maintenance cost of 1?)
-	}
-};
-
-// ##############################################
-// # CIVILIZATIONS                              #
-// ##############################################
-/*
-
-Civ-name-rome "Romani"
-Civ-desc-rome "questa descrizione comparirà nel campo title dove serve ecc."
-
-*/
-
-window.CIVSDB = {
-
-	// ANCIENTS CIVILIATIONS
-
-	'rome': {
-		greeting: 'Ave',
-		leaders: ['Gaius Iulius Cæsar', 'Gaius Iulius Cæsar Octavianus Augustus'], // i nomi dei leaders dovrebbero essere nella loro lingua originale, non tradotti
-		cities: ['Roma', 'Pisa', 'Firenze', 'Venezia'] // lista ordinata di nomi di città per suggerimenti e scelta nomi per le AI
+	civs: {
+		romans: {
+			name: {
+				en: 'Romans',
+				it: 'Romani'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Ave',
+			leaders: ['Gaius Iulius Cæsar', 'Gaius Iulius Cæsar Octavianus Augustus'],
+			cities: ['Roma', 'Pisa', 'Firenze', 'Venezia']
+		},
+		celts: {
+			name: {
+				en: 'Celts',
+				it: 'Celti'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Duw a rhoddodd da i chi',
+			leaders: ['Vercingetorix', 'Boudica'],
+			cities: ['Lutetia', 'Alesia', 'Gergovia']
+		},
+		greeks: {
+			name: {
+				en: 'Greeks',
+				it: 'Greci'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Alexander'],
+			cities: ['Atene']
+		},
+		vikings: {
+			name: {
+				en: 'Vikings',
+				it: 'Vichinghi'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Harald', 'Guglielmo'],
+			cities: []
+		},
+		chinese: {
+			name: {
+				en: 'Chinese',
+				it: 'Cinesi'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Mao'],
+			cities: ['北京 (Beijing)', '上海有 (Shanghai)', '西安 (Xi\'an)']
+		},
+		egyptians: {
+			name: {
+				en: 'Egyptians',
+				it: 'Egizi'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Ramses', 'Tutankamen', 'Cleopatra'],
+			cities: []
+		},
+		germans: {
+			name: {
+				en: 'Germans',
+				it: 'Germanici'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Bismark', 'Barbarossa', 'Hitler'],
+			cities: ['Berlin']
+		},
+		russians: {
+			name: {
+				en: 'Russians',
+				it: 'Russi'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Pietro', 'Katerina', 'Stalin', 'Lenin'],
+			cities: ['Москва (Moskva)']
+		},
+		francs: {
+			name: {
+				en: 'Francs',
+				it: 'Franchi'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Napoleone', 'Giovanna d\'Arco'],
+			cities: []
+		},
+		english: {
+			name: {
+				en: 'English',
+				it: 'Inglesi'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Elisabetta I', 'Vittoria'],
+			cities: []
+		},
+		japanese: {
+			name: {
+				en: 'Japanese',
+				it: 'Giapponesi'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Tokugawa', 'Nobunaga'],
+			cities: []
+		},
+		mongols: {
+			name: {
+				en: 'Mongols',
+				it: 'Mongoli'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Gengis Khan', 'Kubilai Khan'],
+			cities: []
+		},
+		khmer: {
+			name: {
+				en: 'Khmer',
+				it: 'Khmer'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: [],
+			cities: []
+		},
+		native_americans: {
+			name: {
+				en: 'Native Americans',
+				it: 'Nativi Americani'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Hiawatwa'],
+			cities: []
+		},
+		arabs: {
+			name: {
+				en: 'Arabs',
+				it: 'Arabi'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			greeting: 'Hello',
+			leaders: ['Saladino'],
+			cities: []
+		}
 	},
-	'celts': {
-		greeting: 'Duw a rhoddodd da i chi',
-		leaders: ['Vercingetorix', 'Boudica'],
-		cities: ['Lutetia', 'Alesia', 'Gergovia']
-	},
-	'greece': {
-		greeting: 'Hello',
-		leaders: ['Alexander'],
-		cities: ['Atene']
-	},
-	'nordmans': {
-		greeting: 'Hello',
-		leaders: ['Harald', 'Guglielmo'],
-		cities: []
+
+	// BUILDINGS
+
+	buildings: {
+		granary: {
+			name: {
+				en: 'Granary',
+				it: 'Granaio'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			cost: 10,
+			upkeep: 1,
+			require: null
+		}
 	},
 
-	/*
-	Egizi				| 			| 			| Ramses, Tutankamen, Cleopatra
-	Babilonesi			| 			| 			| 
-	Persiani			| Immortale |			| Dario I
-	Maya				|			|			| Pacal
-	Aztechi				|			|			| Montezuma I
-	Incas				|			|			|
-	Indù				|			|			| Ashoka, Ghandi
-	Khmer				|			|			|
-	Cinesi				|			|			| Mao
-	Mongoli				|			|			| Gengis Khan, Kubilai Khan
-	Giapponesi			|			|			| Tokugawa, Nobunaga [Yamamoto]
-	Arabi				| 			| 			| Saladino
-	Nativi americani	| 			| 			| Hiawatwa
-	*/
+	// UNITS
 
-	// MODERN CIVILIZATIONS
-
-	'america': {
-		greeting: 'Hello',
-		leaders: ['George Washington'],
-		cities: ['Washington']
-	},
-	'russia': {
-		greeting: 'Hello',
-		leaders: ['Katerina'],
-		cities: ['Mosca']
-	},
-	'germany': {
-		greeting: 'Hello',
-		leaders: ['Bismark'],
-		cities: ['Berlino']
+	units: {
+		warrior: {
+			name: {
+				en: 'Warrior',
+				it: 'Guerriero'
+			},
+			description: {
+				en: 'Description',
+				it: 'Descrizione'
+			},
+			strength: 1,
+			toughness: 1,
+			movement: 1,
+			cost: 2,
+			require: null,
+			obsolete: null
+		}
 	}
 };
