8c20b7217b82d104fd2dc0905cf9bd427c9ca729
Daniele Veneroni <Venerons@users.noreply.github.com>
2018-10-15 15:59:58 +0200

    Update civis.js

--- js/civis.js
+++ js/civis.js
@@ -62,21 +62,34 @@
 			}
 		}
 
-		var tile_type = ['mountain', 'forest', 'plain', 'desert', 'water'];
+		//var tile_type = ['mountain', 'forest', 'plain', 'desert', 'water'];
 		for (var y = 0; y < map.height; ++y) {
 			for (var x = 0; x < map.width; ++x) {
 				var tile = {
 					id: 'x' + x + 'y' + y,
 					x: x,
 					y: y,
-					type: tile_type[Math.floor(Math.random() * tile_type.length)],
+					type: null, //tile_type[Math.floor(Math.random() * tile_type.length)],
 					fog: true
 				};
+				var number = Math.floor(Math.random() * 100) + 1;
+				if (number >= 1 && number <= 10) {
+					tile.type = 'mountain';
+				} else if (number >= 11 && number <= 30) {
+					tile.type = 'forest';
+				} else if (number >= 31 && number <= 60) {
+					tile.type = 'plain';
+				} else if (number >= 61 && number <= 70) {
+					tile.type = 'desert';
+				} else {
+					tile.type = 'water';
+				}
 				map.tiles[tile.id] = tile;
 			}
 		}
 
-		console.log(map);
+		//console.log(map);
+		return map;
 	};
 
 	Civis.renderMap = function (map) {
@@ -84,8 +97,8 @@
 			width = window.innerWidth,
 			height = window.innerHeight,
 			camera = { x: 0, y: 0 },
-			tileSize = 100,
-			tileGap = 1,
+			tile_size = 100,
+			tile_gap = 1,
 			shapes = {};
 
 		paper.attr({
@@ -103,22 +116,103 @@
 				viewBox: camera.x + ' ' + camera.y + ' ' + width + ' ' + height
 			});
 		});
+		/*
 		paper.mousemove(function (e) {
 			var step = 10;
 			if (e.pageX < 30) { camera.x -= step; }
 			if (e.pageX > width - 30) { camera.x += step; }
 			if (e.pageY < 60) { camera.y -= step; }
 			if (e.pageY > height - 30) { camera.y += step; }
-			paper.attr({
-				viewBox: camera.x + ' ' + camera.y + ' ' + width + ' ' + height
-			});
+			paper.attr({ viewBox: camera.x + ' ' + camera.y + ' ' + width + ' ' + height });
 		});
+		*/
+		paper.drag(function (dx, dy) {
+			camera.x = camera.x_predrag - dx;
+			camera.y = camera.y_predrag - dy;
+			paper.attr({ viewBox: camera.x + ' ' + camera.y + ' ' + width + ' ' + height });
+		}, function (dx, dy) {
+			camera.x_predrag = camera.x;
+			camera.y_predrag = camera.y;
+		}, function (dx, dy) {
+			delete camera.x_predrag;
+			delete camera.y_predrag;
+		});
+
+		var tiles = {
+			mountain: 'img/map/tiles/mountain.jpg',
+			forest: 'img/map/tiles/grass.jpg',
+			plain: 'img/map/tiles/plain.jpg',
+			desert: 'img/map/tiles/desert.jpg',
+			water: 'img/map/tiles/water.jpg'
+		};
+
+		for (var x = 0; x < map.width; ++x) {
+			for (var y = 0; y < map.height; ++y) {
+				var tileID = 'x' + x + 'y' + y;
+				;
+				shapes[tileID] = paper.image(tiles[map.tiles[tileID].type], x * (tile_size + tile_gap), y * (tile_size + tile_gap), tile_size, tile_size);
+				//paper.text(x * (tile_size + tile_gap), y * (tile_size + tile_gap), x + ', ' + y).attr({ 'fill': 'white', 'font-size': 15, 'font-family': 'inherit' });
+			}
+		}
 
 		/*
 		var units;
 		Snap.load('img/units.svg', function (f) {
 			units = f;
 		});
+
+		var renderUnit = function (unit) {
+			//console.log('renderUnit', unit);
+			var size = tile_size * 0.6,
+				x = (unit.x * (tile_size + tile_gap)) + ((tile_size - size) / 2),
+				y = (unit.y * (tile_size + tile_gap)) + ((tile_size - size) / 2),
+				unitSVG = units.select('#unit-' + unit.type).clone(),
+				matrix = Snap.matrix();
+			matrix.translate(x, y);
+			matrix.scale(0.06, 0.06, 0, 0);
+			unitSVG.attr({ fill: MAP.players[unit.player].color, transform: matrix });
+			unitSVG.prepend(paper.rect(0, 0, 1024, 1024).attr({ fill: 'whitesmoke' }));
+			paper.append(unitSVG);
+			shapes[unit.id] = unitSVG;
+			return unitSVG;
+		};
+
+		var renderMove = function (unit, coordX, coordY) {
+			//console.log('renderMove', unit, coordX, coordY);
+			var unitShape = shapes[unit.id],
+				size = tile_size * 0.6,
+				x = (coordX * (tile_size + tile_gap)) + ((tile_size - size) / 2),
+				y = (coordY * (tile_size + tile_gap)) + ((tile_size - size) / 2),
+				matrix = Snap.matrix();
+			matrix.translate(x, y);
+			matrix.scale(0.06, 0.06, 0, 0);
+			unitShape.animate({ transform: matrix }, 500, mina.easeinout);
+		};
+
+		var renderDestination = function (tile, callback) {
+			//console.log('renderDestination', tile, callback);
+			var raggio = tile_size / 4,
+				x = (tile.x * (tile_size + tile_gap)) + (tile_size / 2),
+				y = (tile.y * (tile_size + tile_gap)) + (tile_size / 2),
+				circle = paper.circle(x, y, raggio).attr({
+					'stroke': 'yellow',
+					'stroke-width': 3,
+					'fill': 'gold',
+					'opacity': 0.7,
+					'cursor': 'pointer'
+				}),
+				text = paper.text(x, y, '1').attr({
+					'fill': 'white',
+					'font-size': 20,
+					'font-weight': 'bold',
+					'font-family': 'inherit',
+					'cursor': 'pointer',
+					'text-anchor': 'middle',
+					'dominant-baseline': 'middle'
+				}),
+				destination = paper.group(circle, text);
+			destination.click(callback);
+		};
 		*/
 	};
 
